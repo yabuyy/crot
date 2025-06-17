9e40f4fca3fcabee974391c61f2a99229a94a8c3
yabuyy <162949827+yabuyy@users.noreply.github.com>
2025-06-17 10:38:25 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from "cloudflare:sockets";
 
 // Variables
 const rootDomain = "yabuy.my.id"; // Ganti dengan domain utama kalian
-const serviceName = "crot"; // Ganti dengan nama workers kalian
+const serviceName = ""; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
 const accountID = ""; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
